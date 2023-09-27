dfcce5022dee07e2e42b58db10bb3bfb47a388e2
Divyam <dk0864@srmist.edu.in>
2023-09-28 01:03:32 +0530

    Arr1

---
+++ Array1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[]= {22, 33, 44, 55};
+    for (int i = 0; i < 4; i++)
+    {
+        cout<<"The value of "<<i<<" is:"<<marks[i]<<endl;
+    }
+    
+    return 0;
+}
