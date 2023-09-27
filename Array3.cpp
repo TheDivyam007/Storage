0b0deace8f19b35c2f1620799033f591e91ce071
Divyam <dk0864@srmist.edu.in>
2023-09-28 01:04:34 +0530

    Arr3

---
+++ Array3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[]={34, 54, 45, 49};
+    int i=0;
+    do
+    {
+        cout<<"The value of "<<i<<" is "<<marks[i]<<endl;
+        i++;
+    } while (i<4);
+    
+    return 0;
+} 
