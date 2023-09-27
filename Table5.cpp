cca7214e435d8b01ea5ac5afcc3c000279b7d280
Divyam <dk0864@srmist.edu.in>
2023-09-27 21:16:57 +0530

    5

---
+++ Table5.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"The table of 5 is: "<<endl;
+    for(int i=1; i<=10; i++){
+        cout<<5*i<<endl;
+        // We are multiplying i with 6 so that it will form the table of 5.
+    }
+    return 0;
+}
