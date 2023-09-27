db89bf290ba94e6494946d76153107e08c27575a
Divyam <dk0864@srmist.edu.in>
2023-09-28 01:03:54 +0530

    Arr2

---
+++ Array2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[] = {22, 32, 42, 33, 54};
+    int i=0;
+    while(i<5){
+        cout<<"The value of "<<i<<" is "<<marks[i]<<endl;
+         i++;
+    }
+    return 0;
+}
