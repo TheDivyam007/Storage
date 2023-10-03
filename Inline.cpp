d303265c58b5d261a0d3363f38e537cf860cc220
Divyam <dk0864@srmist.edu.in>
2023-10-03 22:02:47 +0530

    Inline

---
+++ Inline.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+inline int product(int a, int b){
+    return a*b;
+}
+int main(){
+    int a,b;
+    cout<<"The value of a and b is: "<<endl;
+    cin>>a>>b;
+    cout<<"The product of a and b is: "<<product(a,b)<<endl;
+    cout<<"The product of a and b is: "<<product(a,b)<<endl;
+    cout<<"The product of a and b is: "<<product(a,b)<<endl;
+    cout<<"The product of a and b is: "<<product(a,b)<<endl;
+    cout<<"The product of a and b is: "<<product(a,b)<<endl;
+    cout<<"The product of a and b is: "<<product(a,b)<<endl;
+    cout<<"The product of a and b is: "<<product(a,b)<<endl;
+    cout<<"The product of a and b is: "<<product(a,b)<<endl;
+    cout<<"The product of a and b is: "<<product(a,b)<<endl;
+    cout<<"The product of a and b is: "<<product(a,b)<<endl;
+    return 0;
+}
