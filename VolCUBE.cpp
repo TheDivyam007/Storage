7340771f22ec5f454737957a101a29e7bffc0692
Divyam <dk0864@srmist.edu.in>
2023-10-11 08:45:01 +0530

    Cubee

---
+++ VolCUBE.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, volume;
+    cout<<"Enter the side: ";
+    cin>>a;
+    volume = a*a*a;
+    cout<<"The voume of the cube will be: "<<volume;
+    return 0;
+}
