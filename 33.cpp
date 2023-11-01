69b480aef142b5383aa202267c6ac8ba2eec03a0
Divyam <dk0864@srmist.edu.in>
2023-11-01 20:18:41 +0530

    Create 33.cpp

---
+++ 33.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,quo;
+    cout<<"Divide two numbers and print: "<<endl;
+    cout<<"----------------------------"<<endl;
+    cin>>a>>b; 
+    quo = a/b;
+    cout<<"The quotient of "<<a<<" and "<<b<<" is: "<<quo;
+    return 0;
+}
