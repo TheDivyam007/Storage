22b070340329343e584af5ae874bb4c8e636a8a9
Divyam <dk0864@srmist.edu.in>
2023-11-23 21:37:17 +0530

    Create LeapyearCheck.cpp

---
+++ LeapyearCheck.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int year;
+    cout<<"Enter the desired year: "<<endl;
+    cin>>year;
+    if(year%4==0){
+        cout<<"This is a leap year.";
+    }
+    else{
+        cout<<"This is not a leap year.";
+    }
+    return 0;
+}
